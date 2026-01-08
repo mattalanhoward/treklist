afdf8497a08f8af008b4e9639de909f36fd1670a
Matthew Howard <matthewhoward@Matthews-MacBook-Air.local>
2026-01-07 21:54:51 -0500

    Reduced default backgrounds to 4 and added pass giau

--- client/src/config/defaultBackgrounds.js
+++ client/src/config/defaultBackgrounds.js
@@ -7,13 +7,15 @@ import dolomitePeak from "../assets/images/alta-via-2-dolomites-mountain-huts-va
 import dolomiteHuts from "../assets/images/alta-via-2-odle-huts-alpine-landscape-bg.jpg";
 import sasdelechMountainClouds from "../assets/images/alta-via-2-sass-da-lech-mountain-peak-clouds-bg.jpg";
 import dolimiteGrassRidge from "../assets/images/dolomites-alta-via-2-mountain-trail-peak-bg.jpeg";
+import passoGiau from "../assets/images/dolomites-alta-via-1-passo-giau.jpeg";
 
 export const defaultBackgrounds = [
-  { key: "Sierra Nevada", url: sierra },
-  { key: "Grand Canyon", url: grandCanyon },
-  { key: "Zion West Rim", url: zionSnow },
-  { key: "Zion Traverse", url: zionTraverse },
-  { key: "Dolomite Peak", url: dolomitePeak },
+  // { key: "Sierra Nevada", url: sierra },
+  // { key: "Grand Canyon", url: grandCanyon },
+  // { key: "Zion West Rim", url: zionSnow },
+  // { key: "Zion Traverse", url: zionTraverse },
+  // { key: "Dolomite Peak", url: dolomitePeak },
+  { key: "Passo Giau", url: passoGiau },
   { key: "Dolomite Huts", url: dolomiteHuts },
   { key: "Sass de Lech Mountain Clouds", url: sasdelechMountainClouds },
   { key: "Dolomites Grass Ridge", url: dolimiteGrassRidge },
